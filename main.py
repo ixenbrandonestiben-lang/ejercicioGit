859c7fd73885031ac59e5c0818adefb48d378a7b
lestergarcia711 <lestergarcia711@gmail.com>
2026-03-09 08:36:30 -0600

    feat: :sparkles: agregue funcion
    se creo la funcion calcular suma
    
    por @lestergarcia

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ def munu():
         print("4. División")
         print("5. Salir")
     except ValueError:
-        print("Error: Ingrese un número válido.")     
+        print("Error: Ingrese un número válido.") 
+
+def calcular_suma(a,b):
+    num1=float(input("ingrese el primer numero"))
+    num2= float(input("ingrese el segundo numero"))
+    suma = num1 + num2
+    print(suma)
