2585096e031611670c47db761491312dd28d1f89
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-03-09 08:19:10 -0600

    definir menu de calculadora

---
+++ main.py
@@ -0,0 +1,10 @@
+def munu():
+    try:
+        print("   calculadora    ")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Salir")
+    except ValueError:
+        print("Error: Ingrese un número válido.")     
