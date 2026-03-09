03b80a76c2684c1a1f6952df1155c49cd180ec76
lestergarcia711 <lestergarcia711@gmail.com>
2026-03-09 09:01:31 -0600

    feat: :sparkles: estructura logica
    se creo un ciclo para las funciones
    
    por @lestergarcia

--- main.py
+++ main.py
@@ -14,9 +14,25 @@ def calcular_suma(a,b):
     num2= float(input("ingrese el segundo numero"))
     suma = num1 + num2
     print(suma)
-    
+
 def  calcular_division(num1,num2):
     num1=float(input("ingrese el primer numero"))
     num2= float(input("ingrese el segundo numero"))
     producto=num1 * num2
     print(producto)
+while True:
+    munu()
+    opcion= opcion()
+
+    if opcion==1:
+        calcular_suma(num1,num2)
+    elif opcion==2:
+        resta()
+    elif opcion==3:
+        multiplicacion()
+    elif opcion==4:
+        division()
+    elif opcion==4:
+        print("saliendo del programa")
+        break
+  
