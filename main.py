7b0ef90a53094f98506d538a54809c6fa42e29b6
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-03-09 08:28:10 -0600

    rama resta

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ def munu():
         print("5. Salir")
     except ValueError:
         print("Error: Ingrese un número válido.")     
+
+def ocpcion():
+    try:
+        opcion = int(input("Seleccione una opción: "))
+        return opcion
+    except ValueError:
+        print("Error: Ingrese un número válido.")
+        return None
+            
+def resta():
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resultado = num1 - num2
+        print(f"El resultado de la resta es: {resultado}")
+    except ValueError:
+        print("Error: Ingrese un número válido.")
