97f83789b4c7197d5c26670270727846b2f67a9f
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-03-09 08:41:17 -0600

    rama funcion multiplicar

--- main.py
+++ main.py
@@ -25,3 +25,13 @@ def resta():
         print(f"El resultado de la resta es: {resultado}")
     except ValueError:
         print("Error: Ingrese un número válido.")
+        
+def multiplicacion():
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resultado = num1 * num2
+        print(f"El resultado de la multiplicación es: {resultado}")
+    except ValueError:
+        print("Error: Ingrese un número válido.")
+        
